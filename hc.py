deb23ef9d744b723ca1b52882fea356004ccc998
Kedar <kedarkodgire.kk@gmail.com>
2019-02-17 23:51:28 +0530

    started taking the app online

--- hc.py
+++ hc.py
@@ -8,11 +8,11 @@ import copy
 import csv
 
 # Importing the dataset
-dataset = pd.read_csv('Mall_Customers.csv')
+dataset = pd.read_csv("Mall_Customers.csv")
 X = dataset.iloc[:, [3, 4]].values
 # y = dataset.iloc[:, 3].values
 
-new_X=copy.copy(dataset)
+new_X = copy.copy(dataset)
 
 # Splitting the dataset into the Training set and Test set
 """from sklearn.cross_validation import train_test_split
@@ -28,77 +28,58 @@ y_train = sc_y.fit_transform(y_train)"""
 
 # Using the dendrogram to find the optimal number of clusters
 import scipy.cluster.hierarchy as sch
-dendrogram = sch.dendrogram(sch.linkage(X, method = 'ward'))
-plt.title('Dendrogram')
-plt.xlabel('Customers')
-plt.ylabel('Euclidean distances')
+
+dendrogram = sch.dendrogram(sch.linkage(X, method="ward"))
+plt.title("Dendrogram")
+plt.xlabel("Customers")
+plt.ylabel("Euclidean distances")
 plt.show()
 
 # Fitting Hierarchical Clustering to the dataset
 from sklearn.cluster import AgglomerativeClustering
-hc = AgglomerativeClustering(n_clusters = 5,
-                             affinity = 'euclidean',
-                             linkage = 'ward')
+
+hc = AgglomerativeClustering(n_clusters=5, affinity="euclidean", linkage="ward")
 y_hc = hc.fit_predict(X)
 
-#Create new column and save the cluster number to it
-new_X['cluster']=y_hc
+# Create new column and save the cluster number to it
+new_X["cluster"] = y_hc
 
 # Get Hign Income Low Spending in seperate list
 """
 .values is necessary for porpouse of Converting it ti the csv 
 file so dont modify that
 """
-highIncomeLowSpending=new_X.loc[new_X['cluster'] == 0].values
+highIncomeLowSpending = new_X.loc[new_X["cluster"] == 0].values
 
 
 # Convert it into csv file
-with open('hILS.csv', 'w' ,newline='') as fp:
-    a = csv.writer(fp, delimiter=',')
+with open("hILS.csv", "w", newline="") as fp:
+    a = csv.writer(fp, delimiter=",")
     a.writerows(highIncomeLowSpending)
 
 # Visualising the clusters
-plt.scatter(X[y_hc == 0, 0],
-            X[y_hc == 0, 1],
-            s = 100,
-            c = 'red',
-            label = 'Cluster 1')
+plt.scatter(X[y_hc == 0, 0], X[y_hc == 0, 1], s=100, c="red", label="Cluster 1")
 
-plt.scatter(X[y_hc == 1, 0],
-            X[y_hc == 1, 1],
-            s = 100,
-            c = 'blue',
-            label = 'Cluster 2')
+plt.scatter(X[y_hc == 1, 0], X[y_hc == 1, 1], s=100, c="blue", label="Cluster 2")
 
-plt.scatter(X[y_hc == 2, 0],
-            X[y_hc == 2, 1],
-            s = 100,
-            c = 'green',
-            label = 'Cluster 3')
+plt.scatter(X[y_hc == 2, 0], X[y_hc == 2, 1], s=100, c="green", label="Cluster 3")
 
-plt.scatter(X[y_hc == 3, 0],
-            X[y_hc == 3, 1],
-            s = 100, c = 'cyan',
-            label = 'Cluster 4')
+plt.scatter(X[y_hc == 3, 0], X[y_hc == 3, 1], s=100, c="cyan", label="Cluster 4")
 
-plt.scatter(X[y_hc == 4, 0],
-            X[y_hc == 4, 1],
-            s = 100,
-            c = 'magenta',
-            label = 'Cluster 5')
+plt.scatter(X[y_hc == 4, 0], X[y_hc == 4, 1], s=100, c="magenta", label="Cluster 5")
 
 
 ############Some Tests For Errors Please dont change code below###########
-one=X[y_hc == 0, 0], X[y_hc == 0, 1]
-first,second = one
-Y=pd.DataFrame(X,y_hc)
+one = X[y_hc == 0, 0], X[y_hc == 0, 1]
+first, second = one
+Y = pd.DataFrame(X, y_hc)
 ##########################################################################
 
 
-plt.title('Clusters of customers')
-plt.xlabel('Annual Income (k$)')
-plt.ylabel('Spending Score (1-100)')
+plt.title("Clusters of customers")
+plt.xlabel("Annual Income (k$)")
+plt.ylabel("Spending Score (1-100)")
 plt.legend()
-plt.savefig('highincome-lowspending.pdf')
-plt.savefig('highincome-lowspending.png')
+plt.savefig("highincome-lowspending.pdf")
+plt.savefig("highincome-lowspending.png")
 plt.show()
